6bfd377e5053bdca6cce6bfd4a4ee07d2167f556
JeremyPerezSwift <jeremyperez34000@gmail.com>
2023-11-01 09:09:54 +0100

    Initial Commit

---
+++ UberSwiftUIClone/UberSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUICloneApp.swift
+//  UberSwiftUIClone
+//
+//  Created by Jérémy Perez on 01/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
